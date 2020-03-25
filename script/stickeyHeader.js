9a80a8d1d7a702f16aa3c904eff839b398407818
vivekaindia <vvksindia@gmail.com>
2020-03-26 00:21:30 +0530

    feat(Content): Added new content
    Signed-off-by: vivekaindia <vvksindia@gmail.com>

--- script/stickeyHeader.js
+++ script/stickeyHeader.js
@@ -1,12 +1,31 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("head");
-var sticky = header.offsetTop;
+// var header = document.getElementById("head");
+// var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     document.getElementById("menuName").style.width = "100%";
+//     document.getElementById("menuName").style.display = "";
+//   } else {
+//     document.getElementById("menuName").style.width = "0px";
+//     document.getElementById("menuName").style.display = "100%";
+//   }
+// }
+
+$('.menuName').ready(function() {
+  $('.menuName').hide();
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop()>100)
+   {
+     $('#menu').css('background-size',"100%");
+     $('.menuName').show(200);
+   }
+  else
+   {
+    $('.menuName').hide(200);
+    $('#menu').css('background',"");
+   }
+  });
