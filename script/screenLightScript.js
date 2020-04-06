8e1196925979cbb838fe7641631d03f5ff1c28d6
vivekaindia <vvksindia@gmail.com>
2020-04-06 16:00:05 +0530

    fix(UI-background): fixed ui for better starting
    Signed-off-by: vivekaindia <vvksindia@gmail.com>

--- script/screenLightScript.js
+++ script/screenLightScript.js
@@ -1,7 +1,12 @@
 $("#screenLight").ready(function() {
 
     const $screenLight = $('#screenLight');
-    $('#screenLight').val(5);
+    $('#screenLight').val(50);
+
+    var value = parseInt($screenLight.val());
+    value = value/100;
+    var newValue = "linear-gradient(rgba(0, 12, 30, "+value+"), rgba(0, 12, 30, "+value+"))";
+    $(".bodyContent").css('background', newValue);
 
     $screenLight.on('input change', () => {
 
