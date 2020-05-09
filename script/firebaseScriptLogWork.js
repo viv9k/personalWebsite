cad1584c3ee3fd711a7469f6b2f11ecd5a196041
vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>
2020-05-10 00:28:38 +0530

    fix(CreationDate):fixed naming for creationDate in db
    Signed-off-by: vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>

--- script/firebaseScriptLogWork.js
+++ script/firebaseScriptLogWork.js
@@ -18,14 +18,14 @@ firebase.analytics();
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAoeT6sC6LyFcoNLXVc6SG6e3aqBRhsqQg",
-  authDomain: "workloggingsystem.firebaseapp.com",
-  databaseURL: "https://workloggingsystem.firebaseio.com",
-  projectId: "workloggingsystem",
-  storageBucket: "workloggingsystem.appspot.com",
-  messagingSenderId: "744010877071",
-  appId: "1:744010877071:web:6c991081d7b21818b888fa",
-  measurementId: "G-3CWF3NRSW6"
+    apiKey: "AIzaSyAoeT6sC6LyFcoNLXVc6SG6e3aqBRhsqQg",
+    authDomain: "workloggingsystem.firebaseapp.com",
+    databaseURL: "https://workloggingsystem.firebaseio.com",
+    projectId: "workloggingsystem",
+    storageBucket: "workloggingsystem.appspot.com",
+    messagingSenderId: "744010877071",
+    appId: "1:744010877071:web:6c991081d7b21818b888fa",
+    measurementId: "G-3CWF3NRSW6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -54,121 +54,115 @@ var database = firebase.database();
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
-callbacks: {
-signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-// User successfully signed in.
-// Return type determines whether we continue the redirect automatically
-// or whether we leave that to developer to handle.
-return true;
-},
-uiShown: function() {
-// The widget is rendered.
-// Hide the loader.
-document.getElementById('loader').style.display = 'none';
-}
-},
-// Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-signInFlow: 'popup',
-signInSuccessUrl: 'workLoggingSystem.html',
-signInOptions: [
-// Leave the lines as is for the providers you want to offer your users.
-{
-provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-scopes: [
-        'https://www.googleapis.com/auth/contacts.readonly'
-    ],
-},
-firebase.auth.EmailAuthProvider.PROVIDER_ID
-]
-// Terms of service url.
-// tosUrl: '<your-tos-url>',
-// // Privacy policy url.
-// privacyPolicyUrl: '<your-privacy-policy-url>'
-};  
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'workLoggingSystem.html',
+    signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            {
+                provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                scopes: [
+                    'https://www.googleapis.com/auth/contacts.readonly'
+                ],
+            },
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
+        // Terms of service url.
+        // tosUrl: '<your-tos-url>',
+        // // Privacy policy url.
+        // privacyPolicyUrl: '<your-privacy-policy-url>'
+};
 
-function createNewTask(title,des,priority,difficulty,creator,assignee,estimatedTime,status,category,creationDate) {
+function createNewTask(title, des, priority, difficulty, creator, assignee, estimatedTime, status, category, creationDate) {
 
-  var taskIdNumber = getIdNumber();
-  var taskId = category[0]+taskIdNumber;
-  var loggedWorkTotalTime = 0;
-  var workDone = 0;
+    var taskIdNumber = getIdNumber();
+    var taskId = category[0] + taskIdNumber;
+    var loggedWorkTotalTime = 0;
+    var workDone = 0;
 
-  database.ref('Tasks/' + taskId).set({
-    Title: title,
-    Description: des,
-    Priority: priority,
-    Difficulty: difficulty,
-    Creator: creator,
-    Assignee: assignee,
-    ET: estimatedTime,
-    Status: status,
-    Category: category,
-    LogWorkTotalTime: loggedWorkTotalTime,
-    Date: creationDate,
-    WorkDone: workDone
-  });
+    database.ref('Tasks/' + taskId).set({
+        Title: title,
+        Description: des,
+        Priority: priority,
+        Difficulty: difficulty,
+        Creator: creator,
+        Assignee: assignee,
+        ET: estimatedTime,
+        Status: status,
+        Category: category,
+        LogWorkTotalTime: loggedWorkTotalTime,
+        CreationDate: creationDate,
+        WorkDone: workDone
+    });
 
-  newTaskCreatedSuccessfully();
-}
-
-function getIdNumber()
-{
-  var today = new Date();
-  var date = today.getFullYear()+""+(today.getMonth()+1)+""+today.getDate();
-  var time = today.getHours() + "" + today.getMinutes() + "" + today.getSeconds();
-
-  var result = date + time;
-
-  return result;
+    newTaskCreatedSuccessfully();
 }
 
-function newTaskCreatedSuccessfully()
-{
-  $("#createNewTask").fadeOut(100);
-  $("#successful").fadeIn(0);
-  $("#successful").fadeOut(2000);
-  $("#bodyContent").fadeIn(2000);
+function getIdNumber() {
+    var today = new Date();
+    var date = today.getFullYear() + "" + (today.getMonth() + 1) + "" + today.getDate();
+    var time = today.getHours() + "" + today.getMinutes() + "" + today.getSeconds();
+
+    var result = date + time;
+
+    return result;
 }
 
-function getTasks()
-{
-  var result = "ok";
-
-  result = getTasksData();
-
-  return result;
+function newTaskCreatedSuccessfully() {
+    $("#createNewTask").fadeOut(100);
+    $("#successful").fadeIn(0);
+    $("#successful").fadeOut(2000);
+    $("#bodyContent").fadeIn(2000);
 }
 
-function getTasksData()
-{
-  var result = "ok";
+function getTasks() {
+    var result = "ok";
 
-  document.getElementById("jobTickets").innerHTML = "";
-  document.getElementById("taskTickets").innerHTML = "";
+    result = getTasksData();
 
-  var taskData = database.ref('Tasks/');
-  taskData.on('child_added', function(snapshot) {
-    createInstance(snapshot.key, snapshot.val());
-    setIntoTaskCard(snapshot.key);
-  });
+    return result;
+}
 
-  return result;
+function getTasksData() {
+    var result = "ok";
+
+    document.getElementById("jobTickets").innerHTML = "";
+    document.getElementById("taskTickets").innerHTML = "";
+
+    var taskData = database.ref('Tasks/');
+    taskData.on('child_added', function(snapshot) {
+        createInstance(snapshot.key, snapshot.val());
+        setIntoTaskCard(snapshot.key);
+    });
+
+    return result;
 }
 
 var datalist = [];
 
-function createInstance(id, data)
-{
-  datalist[id] = new Data(data);
+function createInstance(id, data) {
+    datalist[id] = new Data(data);
 }
 
-function updateFromLogWorkData(id, logWorkTotalTime, status, workDone)
-{
-  var updates = {};
-  updates['/Tasks/' + id + '/LogWorkTotalTime'] = logWorkTotalTime;
-  updates['/Tasks/' + id + '/Status'] = status;
-  updates['/Tasks/' + id + '/WorkDone'] = workDone;
+function updateFromLogWorkData(id, logWorkTotalTime, status, workDone) {
+    var updates = {};
+    updates['/Tasks/' + id + '/LogWorkTotalTime'] = logWorkTotalTime;
+    updates['/Tasks/' + id + '/Status'] = status;
+    updates['/Tasks/' + id + '/WorkDone'] = workDone;
 
-  return database.ref().update(updates);
+    return database.ref().update(updates);
 
 }
